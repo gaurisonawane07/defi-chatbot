49a9652fc7770264d168e21f992e1be37d3c525a
gauri Sonawane <gaurisonawane918@gmail.com>
2025-06-18 17:53:36 +0530

    Added Required Keys

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config(); 
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.19", 
+
+
+  networks: {
+    sepolia: {
+      url: process.env.RPC_URL , 
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111, 
+      
+    },
+   
+  },
+   etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY, 
+    },
+  },
+
+ 
 };
